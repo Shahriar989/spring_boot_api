14054420a4030963d4a97113348f902868bbbc16
Shahriar <https://github.com/Shahriar989/E-Cart.git>
2023-03-12 22:18:06 +0600

    init

--- src/main/java/com/shahriar/maad/api/controllers/UserController.java
+++ src/main/java/com/shahriar/maad/api/controllers/UserController.java
@@ -5,10 +5,9 @@ import com.shahriar.maad.api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/api")
@@ -22,5 +21,13 @@ public class UserController {
 
         User savedUser = userService.crateUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUser(){
+
+        List<User> users = userService.getAllUser();
+
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
